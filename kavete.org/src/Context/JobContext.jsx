cb2eee1a735bff19efeba9ce86e3728bc5895968
kavetech2023 <kavetech@gmail.com>
2024-08-11 02:17:43 +0300

    feat: Add lazy loading for images on Job page

--- kavete.org/src/Context/JobContext.jsx
+++ kavete.org/src/Context/JobContext.jsx
@@ -1,7 +1,43 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import {jobs} from "../Data/data";
+
 
 export const JobContext = createContext();
 
 const JobContextProvider = (props) =>{
 
+    const [allJobs, setAllJobs] = useState([])
+    const [language, setLanguage] = useState({
+        languages: "English",
+        languageCode: "en"
+    })
+
+    const fetchJobs = async () => {
+        try {
+          setAllJobs(jobs);
+          console.log('Jobs fetched:', jobs);
+        } catch (error) {
+          console.error('Error fetching jobs:', error);
+        }
+      };
+
+    useEffect(() => {
+        fetchJobs();
+    }, [language]);
+
+    const contextValue ={
+        allJobs,
+        language,
+        setLanguage
+    }
+
+    return (
+        <JobContext.Provider value={contextValue}>
+            {props.children}
+        </JobContext.Provider>
+
+    )
+
 }
+
+export default JobContextProvider;
