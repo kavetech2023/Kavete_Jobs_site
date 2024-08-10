634bada1135978da8ae8841f1eb31fc2be652dac
kavetech2023 <kavetech@gmail.com>
2024-08-11 02:17:57 +0300

    feat: Add data.js file with job objects

---
+++ kavete.org/src/Data/data.js
@@ -0,0 +1,37 @@
+export const jobs = [
+    {   
+        id: 1,
+        title: "Software Engineer",
+        location: "San Francisco",
+        company: "ABC Tech",
+        salary: "$100,000",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 2,
+        title: "Web Developer",
+        location: "New York",
+        company: "XYZ Solutions",
+        salary: "$80,000",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    // Add more job objects here...
+    {   id: 3,
+        title: "Data Scientist",
+        location: "Seattle",
+        company: "123 Analytics",
+        salary: "$120,000",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 4,
+        title: "UX Designer",
+        location: "Los Angeles",
+        company: "Design Co.",
+        salary: "$90,000",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    // Add more job objects here...
+];
+
+export default jobs;
