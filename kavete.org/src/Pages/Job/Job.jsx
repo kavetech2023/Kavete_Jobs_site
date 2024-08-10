95e928a9d0cd465c4d952c5bf9aeb81b48100393
kavetech2023 <kavetech@gmail.com>
2024-08-11 02:16:36 +0300

    feat: Add lazy loading for images on Job page

--- kavete.org/src/Pages/Job/Job.jsx
+++ kavete.org/src/Pages/Job/Job.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import './Job.css'
+import React, { useEffect } from "react";
+import "./Job.css";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { jobs } from '../../Data/data'
+import Charts from "../../Components/Charts/Charts";
 
 const Job = () => {
-  return (
-    <div><h2>Job</h2></div>
-  )
-}
+  const { jobId } = useParams();
+  const [jobData, setJobData] = useState({});
 
-export default Job
+  const fetchJobData = async () => {
+    const job = jobs.find(item => item.id == jobId);
+    if (job) {
+      console.log('Job fetched:', job);
+      setJobData(job);
+    } else {
+      console.log('Job not found');
+    }
+  }
+
+  useEffect(() => {
+    fetchJobData();
+  }, [jobId]); // Add jobId as a dependency to re-fetch data when it changes
+
+  if (!jobData) {
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="job">
+        <div className="job-name">
+          <img src="" alt="" />
+          <p><b>{jobData.id}</b></p>
+        </div>
+        <div className="job-chart">
+          <Charts salaryData={jobData}/>
+        </div>
+        <div className="job-info">
+          <ul>
+            <li>Job title</li>
+            <li>Job salary</li>
+          </ul>
+          <ul>
+            <li>Job title</li>
+            <li>Job salary</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Job;
