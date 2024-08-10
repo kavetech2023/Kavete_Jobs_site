98e1bdb41bef8a36852ee4b62c516f0bc06f0f0d
kavetech2023 <kavetech@gmail.com>
2024-08-11 02:15:48 +0300

    feat: Improve responsive design of Navbar component

--- kavete.org/src/Components/Navbar/Navbar.jsx
+++ kavete.org/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
+import { JobContext } from "../../Context/JobContext";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { setlanguage } = useContext(JobContext);
+
+  const languageChange = (e) => {
+    switch (e.target.value) {
+      case "En":
+        setlanguage({
+          languages: "English",
+          languageCode: "en",
+        });
+        break;
+      case "Swh":
+        setlanguage({
+          languages: "Swahili",
+          languageCode: "swh",
+        });
+        break;
+      default:
+        setlanguage({
+          languages: "English",
+          languageCode: "en",
+        });
+        break;
+    }
+  };
   return (
     <div className="navbar">
-      <img src="" alt="Logo" className="logo"/>
+      <Link to={"/"}>
+        <img src="" alt="Logo" className="logo" />
+      </Link>
+
       <ul>
-        <li>Home</li>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact us</li>
+        <Link to={"/"}>
+          <li>Home</li>
+        </Link>
+        <Link to={"/features"}>
+          <li>Features</li>
+        </Link>
+        <Link to={"/pricing"}>
+          <li>Pricing</li>
+        </Link>
+        <Link to={"/about"}>
+          <li>About</li>
+        </Link>
+        <Link to={"/contact"}>
+          <li>Contact us</li>
+        </Link>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={languageChange}>
           <option value="En">English</option>
           <option value="Swh">Swahili</option>
         </select>
-        <button>Sign up <span>+</span></button>
+        <button>
+          Sign up <span>+</span>
+        </button>
       </div>
     </div>
   );
