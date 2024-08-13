0df996ca8d702aab52535ff0dc86102fc48b1054
kavetech2023 <kavetech@gmail.com>
2024-08-13 10:48:42 +0300

    Remove PlayVideo component and associated CSS

--- kavete.org/src/App.jsx
+++ kavete.org/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import AnimatedRoutes from "./AnimatedRoutes";
-import PlayVideo from "./Components/PlayVideo/PlayVideo";
+
 
 const App = () => {
 
