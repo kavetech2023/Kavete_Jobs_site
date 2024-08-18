4b139b571d8f7da70443c77c152f74ee8d8b4c0a
kavetech2023 <kavetech@gmail.com>
2024-08-18 23:53:30 +0300

    feat: Update Job component to use JobContext for data retrieval

--- kavete.org/src/Pages/Job/Job.jsx
+++ kavete.org/src/Pages/Job/Job.jsx
@@ -1,118 +1,122 @@
-import React, { useEffect,useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Job.css";
-import { useParams } from "react-router-dom";
-import { jobs } from "../../Data/data";
+import { Link, useParams } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
-import Navbar from "../../Components/Navbar/Navbar";
+import { JobContext } from "../../Context/JobContext";
 
-
-const Job = ({setNotification}) => {
+const Job = ({ setNotification }) => {
   const { jobId } = useParams();
   const [jobData, setJobData] = useState({});
   const [savedJobs, setSavedJobs] = useState("Add Job");
   const saveJob = () => {
-
     setNotification(+1);
     setSavedJobs("Job Added");
   };
 
-  const fetchJobData = async () => {
-    const job = jobs.find((item) => item.id == jobId);
-    if (job) {
-      console.log("Job fetched:", job);
-      setJobData(job);
-    } else {
-      console.log("Job not found");
-    }
-  };
+  const { jobs } = useContext(JobContext);
+
+
 
   useEffect(() => {
-    fetchJobData();
-  }, [jobId]); // Add jobId as a dependency to re-fetch data when it changes
+    setJobData(jobs);
+ 
+  }, [jobs]);
 
-  if (!jobData) {
-    return (
-      <div className="spinner">
-        <div className="spin"></div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="fadein">
-        <div className="hero">
-          <h1>{jobData.title}</h1>
-          <p>
-            Welcome to the Africa's largest Jobs board. Sign up to explore more
-            about jobs.
-          </p>
-          <button onClick={saveJob} className="btn">{savedJobs}  <FaHeart color={"red"} /> </button>
+    
+ 
+
+  return (
+    <div className="fadein">
+
+      <div className="hero">
+        <h1>Job Details</h1>
+        <p>
+          Look at the most popular price categories of your liking. May they be
+          helpful to you..
+        </p>
+        <div style={{display:"flex",flexDirection:"row",gap:"20px"}}>
+        <button > Update with Ai</button>
+        <button > Print</button>
         </div>
+        
+      </div>
 
-        <div className="app-box">
+      <div className="app-box">
+
+        {jobData && Object.keys(jobData).length > 0 ? (
+          <>
           <div className="job-info">
             <ul>
               <li>
                 <b>Job title</b>
               </li>
-              <li>{jobData.title}</li>
+              <li>{jobData[jobId -1].name}</li>
             </ul>
             <ul>
               <li>
                 <b>Company</b>
               </li>
-              <li>{jobData.company}</li>
+              <li>{jobData[jobId -1].company.name}</li>
             </ul>
             <ul>
               <li>
                 <b>Location</b>
               </li>
-              <li>{jobData.location}</li>
+              <li>{jobData[jobId -1].locations[0].name}</li>
             </ul>
             <ul>
               <li>
-                <b>Qualifications:</b>
+                <b>Job- Level:</b>
               </li>
-              <li>{jobData.minimum_qualifications}</li>
+              <li>
+                {jobData[jobId -1].levels[0].name}
+              </li>
             </ul>
             <ul>
               <li>
-                <b>Preferred Qualifications:</b>
+                <b>Date Published:</b>
               </li>
-              <li>{jobData.preferred_qualifications}</li>
+              <li>{new Date(jobData[jobId].publication_date).toLocaleDateString()}</li>
             </ul>
             <ul>
               <li>
-                <b>Responsibilities:</b>
+                <b>Link to:</b>
               </li>
-              <li>{jobData.responsibilities}</li>
+              <li><Link to={`${jobData[jobId -1].refs.landing_page}`}>Page</Link></li>
             </ul>
             <ul>
               <li>
                 <b>Benefits:</b>
               </li>
-              <li>{jobData.benefits}</li>
+              <li></li>
             </ul>
             <ul>
               <li>
                 <b>Perks:</b>
               </li>
-              <li>{jobData.perks}</li>
+              <li></li>
             </ul>
             <ul>
               <li>
                 <b>Link:</b>
               </li>
               <li>
-                <a href={`${jobData.link}`}>Go to Page</a>
+                <a>Go to Page</a>
               </li>
             </ul>
-
-            
           </div>
-        </div>
+
+          <div className="job-contents">
+          <div style={{flexDirection:"column",display:"flex"}} dangerouslySetInnerHTML={{ __html: jobData[jobId -1].contents }} />
+          </div>
+          </>
+        ) : (
+          <p>No job data available</p>
+        )}
+        
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Job;
