e8b2f71b2e36927e6d7f28dbd5ce03eb4eea487f
kavetech2023 <kavetech@gmail.com>
2024-08-18 10:30:21 +0300

    feat: Add firebase configuration file

---
+++ kavete.org/src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaW5KcSMIZRd-x1ENtCs7YXt5ExEQda8I",
+  authDomain: "kaveteorg-4ffc3.firebaseapp.com",
+  projectId: "kaveteorg-4ffc3",
+  storageBucket: "kaveteorg-4ffc3.appspot.com",
+  messagingSenderId: "179119614250",
+  appId: "1:179119614250:web:de32fe0efbf6c8b814d653",
+  measurementId: "G-EES489235J"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
